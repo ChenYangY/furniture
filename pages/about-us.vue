62f1d734fdcd14e32ab7781569b0b1b1c3ca8d55
ChenYangY <hn2cyy@163.com>
2021-05-30 21:27:23 +0800

    fix(about-us): fix

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -14,8 +14,8 @@
           <div class='left' style='width: 9rem;'><label style='display:block;margin-bottom: 1rem;'>电话</label><span style='font-size: 0.9rem; margin-bottom: 0.5rem; display:block;'>+86 186 7878 0909 </span><span style='font-size: 0.9rem; display: block;width: 100%;'>+86 186 7878 0909 </span></div>
         </div>
       </div>
-      <div style='width: 100%; height: 32.5rem;'></div>
-      <p style='color: #777777'>品牌中最早的一条线，关注寻常生活中真实所需的家居设计，简单质朴，在葆有中国的设计语言的基础上，将大自然中的树木变为与家相融的家具，在四季更迭中长久陪伴，慢慢变成我们的家人。</p>
+      <b-img src='/images/about_us_bg.png' />
+      <p style='color: #777777; margin-top: 2rem;'>品牌中最早的一条线，关注寻常生活中真实所需的家居设计，简单质朴，在葆有中国的设计语言的基础上，将大自然中的树木变为与家相融的家具，在四季更迭中长久陪伴，慢慢变成我们的家人。</p>
       <footer class='page-footer clearfix'>
         <p style='text-align: center;'>2021 © Sigma Design</p>
       </footer>
